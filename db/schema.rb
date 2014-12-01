63d85962e8335467bafe9448a9c5225cbd0481df
brendee <brenda.dargan@gmail.com>
2014-12-01 11:08:53 -0500

    Added Images, updated DB to include kind

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141128183018) do
+ActiveRecord::Schema.define(version: 20141201150050) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "kind"
   end
 
 end
